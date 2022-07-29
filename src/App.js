780f4b05af7ae2d9c1cb206e4b194c03bc022acf
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-29 16:01:54 -0400

    done

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getProductsThunk())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <BrowserRouter>
