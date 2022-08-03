cd4caf55543a9f791d239a6fbf83f67304929751
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-08-03 10:34:20 -0400

    LandingPagedone

---
+++ src/components/LandingPage/LandingPageOne.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import desktop from '../../images/home.jpg'
+import 'animate.css'
+
+export const LandingPageOne = () => {
+  return (
+    <Wrapper>
+      <div className='img-holder'>
+        <img src={desktop} alt='sunglasses ' />
+      </div>
+      <div className='text-holder'>
+        <h2 className='animate__animated animate__fadeInLeft animate__slower'>
+          Prescription <span>Sunglasses</span>
+        </h2>
+        <div className='title-underline'></div>
+        <h5 className='key'>
+          Our affordable sunnies protect your eyes and your wallet all year
+          long.
+        </h5>
+        <Link
+          className='btn btn-hipster animate__animated animate__infinite animate__slower animate__pulse '
+          to='/products'
+        >
+          SHOP ALL
+        </Link>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  .img-holder {
+    max-height: 350px;
+    width: 350px;
+    margin: 0 auto;
+    overflow: hidden;
+    img {
+      max-height: 300px;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+  span {
+    color: var(--primary-5);
+  }
+  /* ==textHolder=== */
+  .text-holder {
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`
---
+++ src/components/LandingPage/LandingPolicy.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LandingPolicy = () => {
+  return (
+    <Wrapper>
+      <div className='box box1'>
+        <h4>40 Million +</h4>
+        <p>Glasses sold</p>
+      </div>
+      <div className='box box2'>
+        <p>TRUSTPILOT</p>
+        <h4>120K + Reviews</h4>
+      </div>
+      <div className='box box3'>
+        <h4>Award-Winning</h4>
+        <p>CUSTOMER SERVICE</p>
+      </div>
+      <div className='box box4'>
+        <h4>30-Day Returns</h4>
+        <p>100 % SATISFACTION GUARANTEED</p>
+      </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+  }
+  gap: 5px;
+  .box {
+    border-bottom: 2px solid var(--primary-5);
+    h4,
+    p {
+      margin: 5px;
+      padding: 0;
+      width: 300px;
+    }
+    h1 {
+    }
+  }
+  .box1 {
+  }
+  .box4 {
+  }
+`
+export default LandingPolicy
