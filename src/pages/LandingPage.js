fb04b5e287a7ee942b23b40ef9d2642a65830610
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-08-03 10:33:42 -0400

    homepage1Done

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+import { LandingPageOne } from '../components/LandingPage/LandingPageOne'
+import LandingPolicy from '../components/LandingPage/LandingPolicy'
 
 const LandingPage = () => {
-  return <div>LandingPage</div>
+  return (
+    <Wrapper>
+      <LandingPageOne />
+      <LandingPolicy />
+    </Wrapper>
+  )
 }
+const Wrapper = styled.section``
 
 export default LandingPage
