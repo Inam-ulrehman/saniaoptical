920b4b9876da8cafed27cfb4475bb63b0bda4153
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-08-04 12:09:15 -0400

    text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,16 @@
 import { React, useEffect } from 'react'
 import styled from 'styled-components'
-import Logo from '../components/Logo'
 
 import 'animate.css'
 import GoogleApi from '../components/GoogleApi'
 
 const About = () => {
-  const aboutUs =
-    'https://res.cloudinary.com/inam6530/image/upload/v1659628330/Beige_Elegant_Premium_Glasses_for_Men_Instagram_Post_1_x6li3l.png'
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
   }, [])
   return (
     <Wrapper>
-      <div className='image-holder'>
-        <img src={aboutUs} alt='' />
-        <div
-          className='hero-text animate__animated 
-        animate__bounceInDown
-        animate__slower'
-        >
-          <Logo />
-        </div>
-      </div>
+      <div className='image-holder'></div>
       <div className='style'></div>
       <GoogleApi />
     </Wrapper>
