04ea254c0e5e8f81c44c30472a35ab0f9c201acc
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-08-04 11:31:54 -0400

    All pages done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,42 @@
 import { React, useEffect } from 'react'
+import styled from 'styled-components'
+import Logo from '../components/Logo'
+import aboutUs from '../images/aboutus.svg'
+import 'animate.css'
+import GoogleApi from '../components/GoogleApi'
 
 const About = () => {
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
   }, [])
-  return <div>About</div>
+  return (
+    <Wrapper>
+      <div className='image-holder'>
+        <img src={aboutUs} alt='' />
+        <div
+          className='hero-text animate__animated 
+        animate__bounceInDown
+        animate__slower'
+        >
+          <Logo />
+        </div>
+      </div>
+      <div className='style'></div>
+      <GoogleApi />
+    </Wrapper>
+  )
 }
-
+const Wrapper = styled.article`
+  .image-holder {
+    position: relative;
+    max-height: 100vh;
+    max-width: 100vw;
+    overflow: hidden;
+    img {
+      margin: 0 auto;
+      max-height: 100vh;
+      width: 100%;
+    }
+  }
+`
 export default About
