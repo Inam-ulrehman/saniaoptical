67efc30bf701417608b6cb90020f5fc15dfda254
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-08-05 09:05:46 -0400

    about done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,30 @@
 import { React, useEffect } from 'react'
 import styled from 'styled-components'
-import GoogleMaps from '../components/GoogleMaps'
-import 'animate.css'
 import GoogleApi from '../components/GoogleApi'
+import { aboutUsData } from '../utils/data'
 
 const About = () => {
+  const aboutImg =
+    'https://firebasestorage.googleapis.com/v0/b/glasshouse-120.appspot.com/o/Untitled%20design.png?alt=media&token=2a21816d-3393-4567-a982-ba5116557360'
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
   }, [])
   return (
     <Wrapper>
-      <div className='image-holder'></div>
-      <GoogleMaps />
+      <div className='image-holder'>
+        <img src={aboutImg} alt='' />
+        <div className='style'></div>
+      </div>
+
+      {aboutUsData.map((item) => {
+        return (
+          <div key={item.id}>
+            <h3 style={{ color: 'var(--primary-5)' }}>{item.heading}</h3>
+            <p>{item.paragraph}</p>
+          </div>
+        )
+      })}
+
       <div className='style'></div>
       <GoogleApi />
     </Wrapper>
@@ -28,6 +41,11 @@ const Wrapper = styled.article`
       max-height: 100vh;
       width: 100%;
       object-fit: cover;
+    }
+  }
+  @media (max-width: 768px) {
+    .image-holder {
+      margin-top: 4rem;
     }
   }
 `
--- src/utils/data.js
+++ src/utils/data.js
@@ -37,3 +37,43 @@ export const gender = [
     link: 'https://res.cloudinary.com/inam6530/image/upload/v1659543186/photo-1651218859577-97abc369d12f_cyltc4.jpg',
   },
 ]
+
+// About us =========
+
+export const aboutUsData = [
+  {
+    id: 1,
+    heading: 'Your Friendly Neighborhood Optical Professionals...',
+    paragraph: `The opening of 20/20 Vision Clinic, located in the heart of downtown Kitchener dates back to 1996, a family-owned and run optical business. With 34 years of experience, we can create the most complex prescriptions and strive to give you an enjoyable and memorable experience.
+
+With the performance of a comprehensive exam, our experts can detect any ocular conditions by offering in-house optometry services, which can be conveniently booked online through our website or by calling the office.
+
+Our store offers a large selection of designer, kids, safety frames, contact lenses, and sunglasses, with over a thousand frames to choose from to fit your personality and lifestyle.
+
+We work hard to accommodate the busy schedules of all our patients; as we are open late hours during the week, open Saturdays, and on-call on Sundays. We strive to ensure each experience is great from start to finish.`,
+  },
+  {
+    id: 2,
+    heading: 'Our Mission',
+    paragraph:
+      'Our goal is to present our customers with the best eye care solutions by providing quality products at affordable prices from our professional and knowledgeable staff. Your satisfaction is important to us.',
+  },
+  {
+    id: 3,
+    heading: 'Great Service',
+    paragraph:
+      'Our professional staff throughout the store will be sure to provide you with 100% customer satisfaction.',
+  },
+  {
+    id: 4,
+    heading: 'High Quality',
+    paragraph:
+      'Seeing is believing. Our showroom has what you are looking for: designer frames, eyeglasses, safety glasses, sunglasses, and contact lenses at a quality that best suits your needs.',
+  },
+  {
+    id: 5,
+    heading: 'Affordable Prices',
+    paragraph:
+      'We offer quality products and extensive services at a price that suits your needs.',
+  },
+]
